3ef17ce60ef676ef1ab4654c930ede76b9c8abda
echizenyayota <virtualmisso30@gmail.com>
2019-02-04 18:14:49 +0900

    リクエストメッセージをオブジェクト変数に格納

--- request.js
+++ request.js
@@ -1,5 +1,16 @@
 const https = require('https');
 
+const options = {
+  protocol: 'https:',
+  host: 'api.paypal.com',
+  path: '/v1/payments/sale/sales_id',
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization' : 'Bearer <Access-Token>'
+  }
+};
+
 const req = https.request(options, (res) => {
     console.log('statusCode:', res.statusCode);
     console.log('headers:', res.headers);
