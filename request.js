b8441b419a1b0d7b2123c2b0ede2d8af9f934afd
echizenyayota <virtualmisso30@gmail.com>
2019-02-04 18:11:39 +0900

    GETメソッドで取得した情報の出力

--- request.js
+++ request.js
@@ -4,6 +4,10 @@ const req = https.request(options, (res) => {
     console.log('statusCode:', res.statusCode);
     console.log('headers:', res.headers);
 
+    res.on('data', (d) => {
+        process.stdout.write(d);
+    });
+
 });
 
 req.on('error', (e) => {
